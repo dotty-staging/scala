faba0777abca80579952bab8403b7dca45b23013
Jiri Vanek <jvanek@redhat.com>
2025-08-07 20:11:21 +0200

    Making natives test working on newer jdk then 8

--- test/files/jvm/natives.scala
+++ test/files/jvm/natives.scala
@@ -4,17 +4,23 @@ object Test {
 
   //println("java.library.path=" + System.getProperty("java.library.path"))
 
+  val userNativelib = System.getenv("scala_test_nativelib") // set to 'natives' for freshly built binary on linux. See mkLibNatives.sh
   val os = System.getProperty("os.name")
   val arch = System.getProperty("os.arch")
+  var libName = ""
 
-  val libName = (os, arch) match {
-    case ("Mac OS X", "aarch64") =>
-      "natives-arm"
-    case ("Mac OS X", "x86_64") =>
-      "natives-x86"
-    case _ =>
-       val wordSize = System.getProperty("sun.arch.data.model", "32")
-      "natives-" + wordSize
+  if (userNativelib == null) {
+    libName = (os, arch) match {
+      case ("Mac OS X", "aarch64") =>
+        "natives-arm"
+      case ("Mac OS X", "x86_64") =>
+        "natives-x86"
+      case _ =>
+         val wordSize = System.getProperty("sun.arch.data.model", "32")
+        "natives-" + wordSize
+    }
+  } else {
+    libName=userNativelib
   }
 
   System.loadLibrary(libName)
