26134ec4f2ed54e950731df89a72e85c8be882c0
Seth Tisue <seth@tisue.net>
2025-08-08 12:30:24 -0700

    minor code improvement in natives test

--- test/files/jvm/natives.scala
+++ test/files/jvm/natives.scala
@@ -4,24 +4,21 @@ object Test {
 
   //println("java.library.path=" + System.getProperty("java.library.path"))
 
-  val userNativelib = System.getenv("scala_test_nativelib") // set to 'natives' for freshly built binary on linux. See mkLibNatives.sh
-  val os = System.getProperty("os.name")
-  val arch = System.getProperty("os.arch")
-  var libName = ""
-
-  if (userNativelib == null) {
-    libName = (os, arch) match {
-      case ("Mac OS X", "aarch64") =>
-        "natives-arm"
-      case ("Mac OS X", "x86_64") =>
-        "natives-x86"
-      case _ =>
-         val wordSize = System.getProperty("sun.arch.data.model", "32")
-        "natives-" + wordSize
-    }
-  } else {
-    libName=userNativelib
-  }
+  val libName =
+    // set to 'natives' for freshly built binary on linux. See mkLibNatives.sh
+    sys.env.getOrElse("scala_test_nativelib", {
+      val os = sys.props("os.name")
+      val arch = sys.props("os.arch")
+      (os, arch) match {
+        case ("Mac OS X", "aarch64") =>
+          "natives-arm"
+        case ("Mac OS X", "x86_64") =>
+          "natives-x86"
+        case _ =>
+          val wordSize = sys.props.getOrElse("sun.arch.data.model", "32")
+          s"natives-$wordSize"
+      }
+    })
 
   System.loadLibrary(libName)
 
